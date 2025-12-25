649fa2ce8337508658c31eda3a03cb8e32d65e2b
linjing-work <1986705965@qq.com>
2025-12-25 10:49:32 +0800

    修改点赞接口

--- Journal/urls.py
+++ Journal/urls.py
@@ -8,17 +8,16 @@ urlpatterns = [
     path('createComment/', views.createComment, name='创建评论'),
     path('user-favorites/<str:username>/', views.get_user_favorites, name='获取用户收藏列表'),
     path('toggle-favorite/', views.toggle_favorite, name='切换收藏状态'),
+    path('toggle_location_like/', views.toggle_location_like, name='切换地点点赞状态'),
 
 
     
-    path('toggle-like-location/', views.toggle_like_location, name='切换地点点赞状态'),
 
     path('createLocation/', views.createLocation, name='创建地点'),
     path('changeLocation/', views.changeLocation, name='修改调整'),
 
     path('showComment/<int:location_id>/', views.showComment, name='展示评论'),
     path('likeComment/', views.likeComment, name='点赞评论'),
-    path('toggle-like-location/', views.toggle_like_location, name='切换地点点赞状态'),
     path('api/nearby-locations/', views.get_nearby_locations, name='获取附近地点'),
     
     # 收藏相关接口
