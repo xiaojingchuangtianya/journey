c2d1d3c035aef90a6b9c83624d6e09eebab2b1dc
linjing-work <1986705965@qq.com>
2026-02-27 11:22:42 +0800

    优化时区配置

--- Journal/admin.py
+++ Journal/admin.py
@@ -1,161 +1,58 @@
 from django.contrib import admin
 from Journal import models
 from django.utils import timezone
-from datetime import timedelta
 
-class UserAdmin(admin.ModelAdmin):
+class BaseAdmin(admin.ModelAdmin):
+    """基础Admin类，包含通用功能"""
+    list_per_page = 20  # 每页显示20条记录
+    ordering = ['-created_at']  # 默认按创建时间倒序排列
+
+class UserAdmin(BaseAdmin):
     """用户模型的Admin类"""
-    list_display = ['username', 'nickname', 'display_china_time']
+    list_display = ['username', 'nickname', 'gender', 'isGetAvatar', 'isGetNickname', 'created_at']
+    search_fields = ['username', 'nickname']  # 搜索字段
+    list_filter = ['gender', 'isGetAvatar', 'isGetNickname']  # 过滤字段
     readonly_fields = ['created_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查created_at是否已经是带时区的时间
-            if hasattr(obj.created_at, 'tzinfo') and obj.created_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.created_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.created_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '创建时间'
 
-class LocationAdmin(admin.ModelAdmin):
+class LocationAdmin(BaseAdmin):
     """地点模型的Admin类"""
-    list_display = ['title', 'address', 'user', 'display_china_time']
+    list_display = ['title', 'address', 'user', 'created_at']
+    search_fields = ['title', 'address']  # 搜索字段
+    list_filter = ['user']  # 过滤字段
     readonly_fields = ['created_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查created_at是否已经是带时区的时间
-            if hasattr(obj.created_at, 'tzinfo') and obj.created_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.created_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.created_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '创建时间'
 
-class CommentAdmin(admin.ModelAdmin):
+class CommentAdmin(BaseAdmin):
     """评论模型的Admin类"""
-    list_display = ['content', 'user', 'location', 'display_china_time']
+    list_display = ['content', 'user', 'location', 'created_at']
+    search_fields = ['content']  # 搜索字段
+    list_filter = ['user', 'location']  # 过滤字段
     readonly_fields = ['created_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查created_at是否已经是带时区的时间
-            if hasattr(obj.created_at, 'tzinfo') and obj.created_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.created_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.created_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '创建时间'
 
-class LikeAdmin(admin.ModelAdmin):
+class LikeAdmin(BaseAdmin):
     """点赞模型的Admin类"""
-    list_display = ['user', 'content_type', 'object_id', 'display_china_time']
+    list_display = ['user', 'content_type', 'object_id', 'created_at']
+    list_filter = ['user', 'content_type']  # 过滤字段
     readonly_fields = ['created_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查created_at是否已经是带时区的时间
-            if hasattr(obj.created_at, 'tzinfo') and obj.created_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.created_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.created_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '点赞时间'
 
-class FavoriteAdmin(admin.ModelAdmin):
+class FavoriteAdmin(BaseAdmin):
     """收藏模型的Admin类"""
-    list_display = ['user', 'location', 'display_china_time']
+    list_display = ['user', 'location', 'created_at']
+    list_filter = ['user', 'location']  # 过滤字段
     readonly_fields = ['created_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查created_at是否已经是带时区的时间
-            if hasattr(obj.created_at, 'tzinfo') and obj.created_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.created_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.created_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '收藏时间'
 
-class CommentPhotoAdmin(admin.ModelAdmin):
+class CommentPhotoAdmin(BaseAdmin):
     """评论照片模型的Admin类"""
-    list_display = ['comment', 'display_china_time']
+    list_display = ['comment', 'uploaded_at']
+    list_filter = ['comment']  # 过滤字段
     readonly_fields = ['uploaded_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查uploaded_at是否已经是带时区的时间
-            if hasattr(obj.uploaded_at, 'tzinfo') and obj.uploaded_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.uploaded_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.uploaded_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '上传时间'
+    ordering = ['-uploaded_at']  # 按上传时间倒序排列
 
-class PhotoAdmin(admin.ModelAdmin):
+class PhotoAdmin(BaseAdmin):
     """照片模型的Admin类"""
-    list_display = ['location', 'is_main', 'display_china_time']
+    list_display = ['location', 'is_main', 'uploaded_at']
+    list_filter = ['location', 'is_main']  # 过滤字段
     readonly_fields = ['uploaded_at']
-    
-    def display_china_time(self, obj):
-        """显示东八区时间"""
-        try:
-            # 检查uploaded_at是否已经是带时区的时间
-            if hasattr(obj.uploaded_at, 'tzinfo') and obj.uploaded_at.tzinfo is not None:
-                # 已经带时区，直接转换
-                china_time = obj.uploaded_at + timedelta(hours=8)
-            else:
-                # 不带时区，先设置为UTC，再转换
-                utc_time = timezone.make_aware(obj.uploaded_at, timezone.utc)
-                china_time = utc_time + timedelta(hours=8)
-            return china_time.strftime('%Y-%m-%d %H:%M:%S')
-        except Exception as e:
-            return f"错误: {str(e)}"
-    
-    display_china_time.short_description = '上传时间'
+    ordering = ['-uploaded_at']  # 按上传时间倒序排列
 
 # 注册带自定义Admin类的模型
 admin.site.register(models.User, UserAdmin)
@@ -165,3 +62,8 @@ admin.site.register(models.Like, LikeAdmin)
 admin.site.register(models.Photo, PhotoAdmin)
 admin.site.register(models.Favorite, FavoriteAdmin)
 admin.site.register(models.CommentPhoto, CommentPhotoAdmin)
+
+# 修改admin站点标题
+admin.site.site_header = '露营地管理系统'
+admin.site.site_title = '露营地管理系统'
+admin.site.index_title = '欢迎使用露营地管理系统'
